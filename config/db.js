4669373007c8c347a87e8819bcacaa3a5a97e070
Gudbrandr42 <mark.bullard42@gmail.com>
2019-10-10 22:15:32 -0400

    initial setup

--- config/db.js
+++ config/db.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI'); // get connet info from global vars stored in default.json or productions.json
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI"); // get connet info from global vars stored in default.json or productions.json
 
 const connectDB = async () => {
-    mongoose.connect(db, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: true
+  mongoose
+    .connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: true
     })
-    .then(() => console.log('Con Database Connected!'))
+    .then(() => console.log("Merch Database Connected!"))
     .catch(err => {
-        console.log(err.message);
-        process.exit(1);
-    })
-}
+      console.log(err.message);
+      process.exit(1);
+    });
+};
 
 module.exports = connectDB;
